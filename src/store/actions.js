1ad8b1f08f1a03cf8e2b7e129c3531cd3e67201a
Hyunwoo Jung <chaosphch@gmail.com>
2019-04-20 21:40:30 +0900

    Fix trivial bug

--- src/store/actions.js
+++ src/store/actions.js
@@ -13,7 +13,7 @@ export const _readGroup = ({ groupId }) => {
   return axios.get(`http://localhost:8000/study_groups/${groupId}/`)
 }
 
-export const _loadMeetings = groupId => {
+export const _loadMeetings = ({ groupId }) => {
   return axios({
     url: `http://localhost:8000/study_meetings?groupId=${groupId}`,
     method: 'get',
