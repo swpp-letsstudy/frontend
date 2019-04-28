a5b45d9128c65e3a72d5734e0cd58c3f5538e114
Hyunwoo Jung <chaosphch@gmail.com>
2019-04-29 01:44:39 +0900

    Remove unnecessary codes

--- src/component/LoginForm/index.js
+++ src/component/LoginForm/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { Formik, Form } from 'formik/dist/index'
@@ -7,28 +7,26 @@ import actionCreators from 'store/actions'
 import Field from './LoginField'
 import Button from './LoginButton'
 
-class LoginForm extends Component {
-  render() {
-    const { isLoggedIn, login } = this.props
-    return (isLoggedIn
-        ? <Redirect to='/'/>
-        : <Formik
-            initialValues={{
-              username: '',
-              password: '',
-            }}
-            onSubmit={(values, formActions) => {
-              const {username, password} = values
-              login({username, password})
-            }}
-            render={() =>
-                <Form>
-                  <Field name='username'/>
-                  <Field type='password' name='password'/>
-                  <Button type='submit'>로그인</Button>
-                </Form>}
-        />)
-  }
+const LoginForm = props => {
+  const {isLoggedIn, login} = props
+  return (isLoggedIn
+      ? <Redirect to='/'/>
+      : <Formik
+          initialValues={{
+            username: '',
+            password: '',
+          }}
+          onSubmit={(values, formActions) => {
+            const {username, password} = values
+            login({username, password})
+          }}
+          render={() =>
+              <Form>
+                <Field name='username'/>
+                <Field type='password' name='password'/>
+                <Button type='submit'>로그인</Button>
+              </Form>}
+      />)
 }
 
 const mapStateToProps = state => ({
