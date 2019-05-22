4d9abdabf0485cfb93dade77de34628872ff282a
sangjunhan <manolike@snu.ac.kr>
2019-05-22 15:44:33 +0900

    styleing notice / backbutton

--- src/component/Group/GroupNotice/GroupNoticeDetail/index.js
+++ src/component/Group/GroupNotice/GroupNoticeDetail/index.js
@@ -1,38 +1,57 @@
 import React, { Component } from 'react'
+import Link from './GroupLink'
 
+import routes from 'routes'
 import apis from 'apis'
 
+import Wrapper from 'component/Styles/Wrapper'
+import Title from 'component/Styles/Title'
+
+import Icon from 'component/Styles/Chevron'
+
 class GroupNoticeDetail extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-        notice: {},
+        this.state = {
+            notice: {},
+        }
     }
-  }
 
-  componentDidMount() {
-    const { noticeId, groupId } = this.props
-    apis.readGroupNotice({ noticeId, groupId })
-    .then(response => {
-        console.log(response)
-        this.setState({
-            notice: response.data,
-        })
-    })
-  }
+    componentDidMount() {
+        const { noticeId, groupId } = this.props
+        apis.readGroupNotice({ noticeId, groupId })
+            .then(response => {
+                console.log(response)
+                this.setState({
+                    notice: response.data,
+                })
+            })
+    }
 
-  render() {
-    const { notice } = this.state
-    return (
-      <>
-        <h1>Title</h1>
-        <p>{notice.title}</p>
-        <h1>Contents</h1>
-        <p>{notice.contents}</p>
-      </>
-    )
-  }
+    render() {
+        const { groupId } = this.props
+        const { notice } = this.state
+        return (
+            <>
+                <Wrapper>
+                    <Icon name='chevron left'>
+                        <Link to={{
+                            pathname: routes.GROUP_NOTICE_LIST,
+                            state: { groupId },
+                        }}>GroupNoticeList
+                        </Link>
+                    </Icon>
+
+                    <Title>Title</Title>
+                    {/* <p>{notice.writer.username}</p> */}
+                    <p>{notice.title}</p>
+                    <Title>Contents</Title>
+                    <p>{notice.contents}</p>
+                </Wrapper>
+            </>
+        )
+    }
 }
 
 
