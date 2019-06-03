3dbf07343de6a87c1478cb594b6e26dea8b4cebf
Hyunwoo Jung <chaosphch@gmail.com>
2019-06-03 15:33:18 +0900

    Add test tree data

--- src/component/CloudStorage.js
+++ src/component/CloudStorage.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
 
+const tree = {
+  first: {
+    first_first: null,
+    first_second: {
+      first_second_first: null,
+    },
+  },
+  second: {
+    second_first: null,
+    second_second: null,
+  },
+  third: {
+    third_first: null,
+    third_second: {
+      third_second_first: null,
+      third_second_second: null,
+    }
+  }
+}
+
 class CloudStorage extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      tree: tree,
+    }
+  }
 
   render() {
     return <div></div>
