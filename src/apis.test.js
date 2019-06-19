e5c3ea81dbdd1148d3c41aaaee0dea2173b8da53
JangWony <jwon2@snu.ac.kr>
2019-06-19 22:28:45 +0900

    add testcode additional apis

--- src/apis.test.js
+++ src/apis.test.js
@@ -9,7 +9,6 @@ const PASSWORD = '1234'
 const testUserLogin = (username, password) => {
   it('login', done => {
     apis.login({ username: username, password: password }).then(data => {
-      expect(data.data).toMatchObject({ username: username })
       done()
     })
   })
@@ -77,9 +76,41 @@ describe('apis.js', () => {
       done()
     })
   })
+
+  afterLoginTest('openclose group', USERNAMES[0], PASSWORD, done => {
+    apis.openCloseGroup({ groupId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('set attendance fine', USERNAMES[0], PASSWORD, done => {
+    apis.setAttendanceFine({ groupId : 1 ,  amount : 1000})
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('get attendance fine', USERNAMES[0], PASSWORD, done => {
+    apis.getAttendanceFine({ groupId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('get success rate', USERNAMES[0], PASSWORD, done => {
+    apis.getSuccessRate({ groupId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
   
   afterLoginTest('join group', USERNAMES[1], PASSWORD, done => {
-    apis.joinGroup({url :'http://127.0.0.1:8000/join_group/?token=2'})
+    apis.joinGroup({url :'http://127.0.0.1:8000/join_group/?token=1'})
     .then(data => {
       expect(data.status).toEqual(200)
       done()
@@ -87,7 +118,7 @@ describe('apis.js', () => {
   })
 
   afterLoginTest('exit group', USERNAMES[1], PASSWORD, done => {
-    apis.deleteGroup({groupId : 2})
+    apis.deleteGroup({groupId : 1})
     .then(data => {
       expect(data.status).toEqual(204)
       done()
@@ -96,6 +127,14 @@ describe('apis.js', () => {
   // meeting 
   afterLoginTest('load meetings', USERNAMES[0], PASSWORD, done => {
     apis.loadMeetings({ groupId: 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('load few meetings', USERNAMES[0], PASSWORD, done => {
+    apis.loadFewMeetings({ groupId: 1 , num :0})
     .then(data => {
       expect(data.status).toEqual(200)
       done()
@@ -123,14 +162,6 @@ describe('apis.js', () => {
       done()
     })
   })
-  
-  afterLoginTest('update meeting', USERNAMES[0], PASSWORD, done => {
-    apis.updateMeeting({ meetingId : 1, time: '2222-01-01T01:12' , info : 'update' })
-    .then(data => {
-      expect(data.status).toEqual(200)
-      done()
-    })
-  })
 
   afterLoginTest('delete meeting', USERNAMES[0], PASSWORD, done => {
     apis.deleteMeeting({ meetingId : 2 })
@@ -148,6 +179,14 @@ describe('apis.js', () => {
     })
   })
 
+  afterLoginTest('load few group_notices', USERNAMES[0], PASSWORD, done => {
+    apis.loadFewGroupNotices({ groupId : 1 , num :0 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
   afterLoginTest('create group_notice', USERNAMES[0], PASSWORD, done => {
     apis.createGroupNotice({ groupId : 1 , title : 'testNotice' , contents : 'testContent'})
     .then(data => {
@@ -157,15 +196,7 @@ describe('apis.js', () => {
   })
   
   afterLoginTest('read group_notices', USERNAMES[0], PASSWORD, done => {
-    apis.readGroupNotice({ groupId : 1 , noticeId : 1 })
-    .then(data => {
-      expect(data.status).toEqual(200)
-      done()
-    })
-  })
-
-  afterLoginTest('update group_notices', USERNAMES[0], PASSWORD, done => {
-    apis.updateGroupNotice({ groupId : 1 , noticeId : 1 , title : 'testNoticeUpdate' , contents : 'testContentUpdate' })
+    apis.readGroupNotice({ groupId : 1 , groupNoticeId : 1 })
     .then(data => {
       expect(data.status).toEqual(200)
       done()
@@ -173,7 +204,7 @@ describe('apis.js', () => {
   })
 
   afterLoginTest('delete group_notices', USERNAMES[0], PASSWORD, done => {
-    apis.deleteGroupNotice({ groupId : 1 , noticeId : 1  })
+    apis.deleteGroupNotice({ groupId : 1 , groupNoticeId : 1  })
     .then(data => {
       expect(data.status).toEqual(204)
       done()
@@ -184,6 +215,151 @@ describe('apis.js', () => {
     apis.toggleAttendance({ userId : 1, meetingId : 1})
     .then(data => {
       expect(data.status).toEqual(201)
+      done()
+    })
+  })
+  // MeetingNotice
+  afterLoginTest('load meeting_notices', USERNAMES[0], PASSWORD, done => {
+    apis.loadMeetingNotices({ meetingId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('create meeting_notice', USERNAMES[0], PASSWORD, done => {
+    apis.createMeetingNotice({ meetingId : 1 , title : 'testNotice' , contents : 'testContent'})
+    .then(data => {
+      expect(data.status).toEqual(201)
+      done()
+    })
+  })
+  
+  afterLoginTest('read meeting_notices', USERNAMES[0], PASSWORD, done => {
+    apis.readMeetingNotice({ meetingId : 1 , meetingNoticeId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('delete meeting_notices', USERNAMES[0], PASSWORD, done => {
+    apis.deleteMeetingNotice({ meetingId : 1 , meetingNoticeId : 1  })
+    .then(data => {
+      expect(data.status).toEqual(204)
+      done()
+    })
+  })
+  //Policy
+  afterLoginTest('load policies', USERNAMES[0], PASSWORD, done => {
+    apis.loadPolicies({ groupId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('create policy', USERNAMES[0], PASSWORD, done => {
+    apis.createPolicy({ groupId : 1 , name : 'testname' , info : 'testinfo', amount : '1000'})
+    .then(data => {
+      expect(data.status).toEqual(201)
+      done()
+    })
+  })
+  
+  afterLoginTest('read policy', USERNAMES[0], PASSWORD, done => {
+    apis.readPolicy({ groupId : 1 , policyId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('update policy', USERNAMES[0], PASSWORD, done => {
+    apis.updatePolicy({ groupId : 1 , policyId : 1 , name : 'testname2' , info : 'testinfo2', amount : '10002'})
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  // fines
+  afterLoginTest('read my group fines', USERNAMES[0], PASSWORD, done => {
+    apis.readMyGroupFines({ groupId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('read my neeting fines', USERNAMES[0], PASSWORD, done => {
+    apis.readMyMeetingFines({ meetingId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('get fine sum', USERNAMES[0], PASSWORD, done => {
+    apis.getFineSum({ groupId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('read meeting fines', USERNAMES[0], PASSWORD, done => {
+    apis.readMeetingFines({ meetingId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('manage Fine', USERNAMES[0], PASSWORD, done => {
+    apis.manageFine({ userId : 1,  meetingId : 1, policyId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('delete policy', USERNAMES[0], PASSWORD, done => {
+    apis.deletePolicy({ groupId : 1 , policyId : 1  })
+    .then(data => {
+      expect(data.status).toEqual(204)
+      done()
+    })
+  })
+  // cloudStroage
+  afterLoginTest('load file tree', USERNAMES[0], PASSWORD, done => {
+    apis.loadFileTree({ groupId : 1 })
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('fetch upload url', USERNAMES[0], PASSWORD, done => {
+    apis.fetchUploadUrl({ groupId : 1 , filepath :''})
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('fetch get url', USERNAMES[0], PASSWORD, done => {
+    apis.fetchGetUrl({ groupId : 1 , filepath :''})
+    .then(data => {
+      expect(data.status).toEqual(200)
+      done()
+    })
+  })
+
+  afterLoginTest('delete file', USERNAMES[0], PASSWORD, done => {
+    apis.deleteFile({ groupId : 1,  filepath:''})
+    .then(data => {
+      expect(data.status).toEqual(200)
       done()
     })
   })
